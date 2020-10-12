90bbbe222d32381014a8728d3d9b9a751399a9b2
Ben Laniado <ben.laniado@naturalint.com>
2020-10-12 22:49:10 +0300

    added build:component:vue to the build process (#5)

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <GpEditor />
+  </div>
+</template>
+
+<script>
+import GpEditor from './components/GpEditor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GpEditor
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/GpEditor.vue
@@ -0,0 +1,22 @@
+<template>
+    <ckeditor
+            v-if="editor"
+            v-bind="$attrs"
+            v-on="$listeners"
+            :editor="editor"
+            tag-name="textarea" />
+</template>
+
+<script>
+  import CKEditor from '@ckeditor/ckeditor5-vue';
+  import ClassicEditor from '../../build/ckeditor';
+
+  export default {
+    name: 'GpEditor',
+    components: { ckeditor: CKEditor.component },
+    data: () =>  ({
+      editor: ClassicEditor,
+    }),
+  };
+</script>
+
