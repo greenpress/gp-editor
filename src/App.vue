9af6a71fc1cb010fb27a2701bd658aad4724370e
Ben Laniado <benland123@gmail.com>
2020-10-17 23:12:47 +0300

    config and value properties (#7)
    * explicit config and value attributes
    pre publish only build
    
    * App.vue for testing
    reload editor when config changes
    
    * dist in source dir + index.js filename
    
    * translations folder + index files
    
    * translations and map files

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,35 @@
 <template>
-  <div id="app">
-    <GpEditor />
-  </div>
+    <div id="app">
+        <GpEditor :value="'hello world'" :config="conf" />
+    </div>
 </template>
 
 <script>
-import GpEditor from './components/GpEditor.vue'
+  import GpEditor from './components/GpEditor.vue'
 
-export default {
-  name: 'App',
-  components: {
-    GpEditor
-  }
+  export default {
+    name: 'App',
+    components: {
+      GpEditor
+    },
+    data: () => ({
+      conf: {},
+    }),
+    created: function () {
+      setTimeout( () => {
+        this.conf = {language: 'he'};
+      }, 3000);
+    },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
